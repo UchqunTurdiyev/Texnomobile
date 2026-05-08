fcbd27e01f090d98438a6dd79916d4734fadf79e
Uchqun <83647411+UchqunTurdiyev@users.noreply.github.com>
2026-05-08 01:11:29 -0700

    NEW META PIXEL

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '4399786953677592');
+              fbq('init', '1759492982081848');
               fbq('track', 'PageView');
             `,
           }}
